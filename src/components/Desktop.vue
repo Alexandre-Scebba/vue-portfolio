893bed73d176178890cd939692b9db1013aeb152
Alexandre-Scebba <161493583+Alexandre-Scebba@users.noreply.github.com>
2025-06-02 11:03:11 -0400

    +Examina: link update test

--- src/components/Desktop.vue
+++ src/components/Desktop.vue
@@ -1,54 +1,52 @@
 <template>
-    <div class="desktop">
-      <div class="folder-grid">
-        <div class="folder" v-for="folder in folders" :key="folder.name" @click="openFolder(folder)">
-          <img :src="folder.icon" alt="folder" />
-          <p>{{ folder.name }}</p>
-        </div>
+  <div class="desktop">
+    <div class="folder-grid">
+      <div class="folder" v-for="folder in folders" :key="folder.name" @click="openFolder(folder)">
+        <img :src="folder.icon" alt="folder" />
+        <p>{{ folder.name }}</p>
       </div>
-      
-
-  <!-- Attribution Below the Grid -->
-<div class="attribution">
-  <p>
-    <a href="https://www.flaticon.com/free-icons/folder" title="folder icons">
-      {{ translations[currentLanguage].Attribution }}
-    </a>
-  </p>
-</div>
-
-     <!-- Start Menu Component -->
-     <StartMenu ref="startMenu" />
-
-<!-- Taskbar / Footer INSIDE SCREEN INNER -->
-<div class="screen-inner">
-<footer class="taskbar">
-  <div class="left-side">
-    <button @click="toggleStart">{{ translations[currentLanguage].Start }}</button>
-    <div class="taskbar-divider"></div>
-  </div>
-
-  <div class="right-side">
-    <div class="language-switch">
-      <button @click="toggleLanguage">{{ currentLanguage }}</button>
     </div>
-    <div class="taskbar-divider"></div>
-    <div class="taskbar-clock">
-      {{ currentTime }}
-    </div>
-  </div>
-</footer>
-  </div>
 
-   <!-- Project Modal (Folder View) -->
-   <ProjectModal ref="projectModal" :folderName="'Projects'" :projects="projectList" />
-   <AboutModal :currentLanguage="currentLanguage" ref="aboutModal" />
-   <EmailConfirmModal :currentLanguage="currentLanguage" ref="emailModal" />
+    <!-- Attribution Below the Grid -->
+    <div class="attribution">
+      <p>
+        <a href="https://www.flaticon.com/free-icons/folder" title="folder icons">
+          {{ translations[currentLanguage].Attribution }}
+        </a>
+      </p>
+    </div>
+
+    <!-- Start Menu Component -->
+    <StartMenu ref="startMenu" />
+
+    <!-- Taskbar / Footer INSIDE SCREEN INNER -->
+    <div class="screen-inner">
+      <footer class="taskbar">
+        <div class="left-side">
+          <button @click="toggleStart">{{ translations[currentLanguage].Start }}</button>
+          <div class="taskbar-divider"></div>
+        </div>
+
+        <div class="right-side">
+          <div class="language-switch">
+            <button @click="toggleLanguage">{{ currentLanguage }}</button>
+          </div>
+          <div class="taskbar-divider"></div>
+          <div class="taskbar-clock">
+            {{ currentTime }}
+          </div>
+        </div>
+      </footer>
+    </div>
+
+    <!-- Project Modal (Folder View) -->
+    <ProjectModal ref="projectModal" :folderName="'Projects'" :projects="projectList" />
+    <AboutModal :currentLanguage="currentLanguage" ref="aboutModal" />
+    <EmailConfirmModal :currentLanguage="currentLanguage" ref="emailModal" />
   </div>
 </template>
-  
-  <script setup>
 
+<script setup>
 import { ref } from 'vue'
 import StartMenu from './StartMenu.vue'
 import ProjectModal from './ProjectModal.vue'
@@ -61,14 +59,12 @@ import flashCardIcon from '@/assets/project-icons/FlashCards logo.png'
 import codexIcon from '@/assets/project-icons/CODEX logo.png'
 import examinaIcon from '@/assets/project-icons/examina logo.png'
 
-
-
 const startMenu = ref(null)
 const projectModal = ref(null)
 const aboutModal = ref(null)
 const emailModal = ref(null)
 
-// EN/FR logic 
+// EN/FR logic
 const currentLanguage = ref('EN')
 
 const translations = {
@@ -78,7 +74,7 @@ const translations = {
     AboutMe: 'About Me',
     Contact: 'Contact',
     CV: 'C.V.',
-    Attribution: 'Folder icons created by kmg design - Flaticon'
+    Attribution: 'Folder icons created by kmg design - Flaticon',
   },
   FR: {
     Start: 'Démarrer',
@@ -86,8 +82,8 @@ const translations = {
     AboutMe: 'À propos de moi',
     Contact: 'Contactez-moi',
     CV: 'C.V.',
-    Attribution: 'Icônes de dossier créées par kmg design - Flaticon'
-  }
+    Attribution: 'Icônes de dossier créées par kmg design - Flaticon',
+  },
 }
 
 // Reactive Folders
@@ -95,14 +91,14 @@ const folders = ref([
   { key: 'Projects', icon: 'https://cdn-icons-png.flaticon.com/512/716/716784.png' },
   { key: 'AboutMe', icon: 'https://cdn-icons-png.flaticon.com/512/4021/4021693.png' },
   { key: 'Contact', icon: 'https://cdn-icons-png.flaticon.com/128/2374/2374449.png' },
-  { key: 'CV', icon: 'https://cdn-icons-png.flaticon.com/128/14180/14180779.png' }
+  { key: 'CV', icon: 'https://cdn-icons-png.flaticon.com/128/14180/14180779.png' },
 ])
 
 // Function to Update Folder Names Based on Language
 const updateFolderNames = () => {
-  folders.value = folders.value.map(folder => ({
+  folders.value = folders.value.map((folder) => ({
     ...folder,
-    name: translations[currentLanguage.value][folder.key]
+    name: translations[currentLanguage.value][folder.key],
   }))
 }
 
@@ -115,17 +111,10 @@ const toggleLanguage = () => {
   updateFolderNames()
 }
 
-
-
-  // Toggle Start Menu
+// Toggle Start Menu
 const toggleStart = () => {
   startMenu.value.toggleMenu()
 }
-
-
-
-
-
 
 const currentTime = ref(new Date().toLocaleTimeString())
 
@@ -141,43 +130,43 @@ setInterval(() => {
 ~excuses API
 */
 const projectList = [
-{
+  {
     name: 'Portfolio Website',
     icon: 'https://cdn-icons-png.flaticon.com/128/232/232411.png',
-    link: 'https://alex-scebba-portfolio.vercel.app/'
+    link: 'https://alex-scebba-portfolio.vercel.app/',
   },
   {
     name: 'Examina',
-    icon:  examinaIcon,
-    link: 'https://examina2-cvaehchtdtd4epap.canadaeast-01.azurewebsites.net/'
+    icon: examinaIcon,
+    link: 'https://github.com/m1ndb0x/AppDev2Project',
+    //'https://examina2-cvaehchtdtd4epap.canadaeast-01.azurewebsites.net/'
   },
   {
     name: 'CODEX',
     icon: codexIcon,
-    link: 'https://github.com/vvaraksn/SnippetManager'
+    link: 'https://github.com/vvaraksn/SnippetManager',
   },
   {
     name: 'DragonNotes',
     icon: 'https://cdn-icons-png.flaticon.com/128/669/669250.png',
-    link: 'https://dragon-notes.vercel.app/'
+    link: 'https://dragon-notes.vercel.app/',
   },
   {
     name: 'Clonify',
     icon: clonifyIcon,
-    link: 'https://github.com/yourusername/clonify'
+    link: 'https://github.com/yourusername/clonify',
   },
   {
     name: 'Floppy Code',
     icon: floppyCodeIcon,
-    link: 'https://alexandre-scebba.github.io/FloppyCodePage/'
+    link: 'https://alexandre-scebba.github.io/FloppyCodePage/',
   },
   {
     name: 'Flash Card App',
     icon: flashCardIcon,
-    link: 'https://github.com/Alexandre-Scebba/flashcard-app'
-  }
+    link: 'https://github.com/Alexandre-Scebba/flashcard-app',
+  },
 ]
-
 
 // Open folder modal logic
 const openFolder = (folder) => {
@@ -190,87 +179,79 @@ const openFolder = (folder) => {
 
   if (folder.name === translations[currentLanguage.value].Contact) {
     emailModal.value.openModal()
-}
- if (folder.name === 'C.V.') {
-    const cvFile = currentLanguage.value === 'FR' ? 
-      '/Alex Scebba CV 2025-FR.pdf' : 
-      '/Alex Scebba CV 2025-EN.pdf';
-      
-    window.open(cvFile, '_blank');
+  }
+  if (folder.name === 'C.V.') {
+    const cvFile =
+      currentLanguage.value === 'FR' ? '/Alex Scebba CV 2025-FR.pdf' : '/Alex Scebba CV 2025-EN.pdf'
+
+    window.open(cvFile, '_blank')
   }
 }
+</script>
 
-  </script>
-  
+<style scoped>
+/* ..............................DESKTOP */
+.desktop {
+  min-height: clamp(600px, 90vh, 1000px);
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: space-between;
+  background: url('https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png')
+    no-repeat center center/cover;
+  background-size: cover; /* Maintain aspect ratio while covering */
+}
 
-  <style scoped>
-  /* ..............................DESKTOP */
-  .desktop {
-    min-height: clamp(600px, 90vh, 1000px);
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
-    justify-content: space-between;
-    background: url('https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png') no-repeat center center/cover;
-    background-size: cover;  /* Maintain aspect ratio while covering */
+/* ..............................FOLDER */
+.folder-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  gap: clamp(0.5rem, 1.5vw, 2rem);
+  max-width: 1200px;
+  padding: 2rem;
+  justify-items: start;
+}
 
-  }
-  
+.folder {
+  text-align: center;
+  cursor: pointer;
+  transition: transform 0.2s ease;
+}
 
+.folder img {
+  width: clamp(40px, 5vw, 70px);
+  margin-bottom: 0.5rem;
+}
 
+.folder:hover {
+  transform: scale(1.15);
+}
 
-  /* ..............................FOLDER */
-  .folder-grid {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
-    gap: clamp(0.5rem, 1.5vw, 2rem);
-    max-width: 1200px;
-    padding: 2rem;
-    justify-items: start;
-
-  }
-  
-  .folder {
-    text-align: center;
-    cursor: pointer;
-    transition: transform 0.2s ease;
-  }
-  
-  .folder img {
-    width: clamp(40px, 5vw, 70px);
-    margin-bottom: 0.5rem;
-  }
-  
-  .folder:hover {
-    transform: scale(1.15);
-  }
-
-  .folder p {
+.folder p {
   margin: 0;
   font-size: clamp(0.8rem, 1vw, 1.2rem);
   color: white;
-  text-shadow: 1px 1px 2px black, 0 0 1px rgba(0, 0, 0, 0.8); /* black outline */
+  text-shadow:
+    1px 1px 2px black,
+    0 0 1px rgba(0, 0, 0, 0.8); /* black outline */
   font-weight: normal;
-  font-family: 'Tahoma', sans-serif;  /* classic XP-style font */
+  font-family: 'Tahoma', sans-serif; /* classic XP-style font */
 }
 
-
-
-
-  /* ..............................TASKBAR */
-  .taskbar {
-    width: 100%;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    height: clamp(35px, 5vh, 50px);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background: linear-gradient(to top, #1c5aa6, #2679c3);
-    padding: 0.5rem 0;
-    box-shadow: 0 -3px 5px rgba(0, 0, 0, 0.5);
-    z-index: 10;  /* Keep above other content */
+/* ..............................TASKBAR */
+.taskbar {
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: clamp(35px, 5vh, 50px);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: linear-gradient(to top, #1c5aa6, #2679c3);
+  padding: 0.5rem 0;
+  box-shadow: 0 -3px 5px rgba(0, 0, 0, 0.5);
+  z-index: 10; /* Keep above other content */
 }
 
 .left-side {
@@ -299,7 +280,6 @@ const openFolder = (folder) => {
   font-size: clamp(0.8rem, 1vw, 1rem);
 }
 
-
 .taskbar button:hover {
   background: linear-gradient(to top, #51c33a, #63e045);
 }
@@ -314,7 +294,6 @@ const openFolder = (folder) => {
   margin: 0 clamp(5px, 1vw, 15px);
 }
 
-
 .taskbar-clock {
   color: white;
   margin-left: auto;
@@ -324,15 +303,9 @@ const openFolder = (folder) => {
   white-space: nowrap;
 }
 
+/* ..............................ATTRIBUTIONS */
 
-
-
-
-
-
-  /* ..............................ATTRIBUTIONS */
-
-  .attribution {
+.attribution {
   position: absolute;
   bottom: clamp(40px, 5vh, 80px);
   right: clamp(10px, 2vw, 30px);
@@ -342,7 +315,6 @@ const openFolder = (folder) => {
   color: #bbb;
 }
 
-
 .attribution a {
   color: #bbb;
   text-decoration: none;
@@ -351,8 +323,4 @@ const openFolder = (folder) => {
 .attribution a:hover {
   color: white;
 }
-
-
-
-  </style>
-  
+</style>
